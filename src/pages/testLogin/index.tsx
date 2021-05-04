92262a69b2357120ff3f7ca3b34aef20af00e213
Stickers-hackathon <watanabeso@crunchtimer.jp>
2021-05-04 22:12:00 +0900

    add:firebaseui追加

--- src/pages/testLogin/index.tsx
+++ src/pages/testLogin/index.tsx
@@ -1,9 +1,16 @@
 import React, { useContext, useEffect } from 'react'
 import firebase, { auth } from '../../utils/firebase'
 import { AuthContext } from '../../auth/auth'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
 const Index: React.FC = () => {
   const { currentUser } = useContext(AuthContext)
+
+  const uiConfig = {
+    signInFlow: 'popup',
+    signInSuccessUrl: '/',
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  }
 
   useEffect(() => {
     //TODO:ログイン成功後の処理
@@ -17,7 +24,7 @@ const Index: React.FC = () => {
 
   return (
     <>
-      <button onClick={login}>googleでログインする</button>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
     </>
   )
 }
