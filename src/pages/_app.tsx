45a555dbe41d5896f6e7a8364b84549d3f170663
Yoshiki Fujikane <ffjlabo@gmail.com>
2021-05-18 20:58:31 +0900

    chore: setup material-ui for nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
+import theme from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) jssStyles.parentElement?.removeChild(jssStyles)
+  }, [])
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
---
+++ src/pages/_document.tsx
@@ -0,0 +1,42 @@
+// Use Material-ui with SSR / SSG
+// https://github.com/mui-org/material-ui/tree/master/examples/nextjs
+
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+import theme from '../theme'
+import { ServerStyleSheets } from '@material-ui/styles'
+
+export default class MyDocument extends Document {
+  // @ts-ignore
+  static async getInitialProps(ctx) {
+    const sheets = new ServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // @ts-ignore
+        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+    }
+  }
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
